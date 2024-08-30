c5d66188c835d3c07ac44da21e7cba25105ded0b
Partridge <anhduc130703@gmail.com>
2024-08-30 11:52:19 +0700

    add comment

--- week-1/cmd/main.go
+++ week-1/cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
-import "github.com/partridge1307/week-1/pkg"
+import (
+	"fmt"
+
+	"github.com/partridge1307/week-1/pkg"
+)
 
 func main() {
 	// Race condition
+	fmt.Println("Race condition: ")
 	pkg.RaceCondition()
 	pkg.PreventRaceCondition()
 
 	// Worker pool
+	fmt.Println("\n\nWorker pool")
 	pkg.Workerpool()
 }
